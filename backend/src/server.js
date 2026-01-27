aeb8408fc677385bc71a746cad7c96706904687b
pulkitgaba009 <pulkitgaba009@gmail.com>
2026-01-27 19:34:51 +0530

    deployment config added

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,13 +1,25 @@
 import express from "express";
+import path from "path";
 import tryRouter from "./routes/try.js";
 import { ENV } from "./lib/env.js";
 
 const app = express();
-const port = process.env.PORT;
+const __dirname = path.resolve();
 
 app.use(express.json());
-app.use("/api",tryRouter);
+app.use("/api", tryRouter);
 
-app.listen(port,()=>{
+if (ENV.NODE_ENV === "production") {
+    const frontendPath = path.join(__dirname, "..", "frontend", "dist");
+
+    app.use(express.static(frontendPath));
+
+    // Express 5 safe SPA fallback
+    app.get(/.*/, (req, res) => {
+        res.sendFile(path.join(frontendPath, "index.html"));
+    });
+}
+
+app.listen(ENV.PORT, () => {
     console.log(`App is running at PORT : ${ENV.PORT}`);
-})
+});
