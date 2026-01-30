97d4e92aba709d59c50ab2f5e11953b2f660a5a0
pulkitgaba009 <pulkitgaba009@gmail.com>
2026-01-30 12:34:44 +0530

    serverside auth implemented

---
+++ backend/src/middleware/protectRoute.js
@@ -0,0 +1,29 @@
+import { requireAuth } from "@clerk/express";
+import User from "../models/User.models.js";
+
+export const protectRoute = [
+  requireAuth(),
+  async (req, res, next) => {
+    try {
+      const clerkId = req.auth.userId;
+
+      if (!clerkId) {
+        return res
+          .status(401)
+          .json({ message: "Unauthorized - Invalid token" });
+      }
+
+      const user = await User.findOne({ clerkId });
+
+      if (!user) {
+        return res.status(404).json({ message: "User not found" });
+      }
+
+      req.user = user;
+      next();
+    } catch (error) {
+      console.error("Error in protectRoute middleware:", error);
+      return res.status(500).json({ message: "Internal Server Error" });
+    }
+  },
+];
--- backend/src/routes/try.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { getRoute } from "../controller/controller.js";
-
-const router = express.Router();
-
-router.get("/",getRoute)
-
-export default router
